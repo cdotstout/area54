3e77c702cbccb51582aa2fad3a1f0e2d2de858a5
cstout <castout@gmail.com>
2017-08-06 17:31:10 -0700

    Add flash instructions

--- build.sh
+++ build.sh
@@ -9,3 +9,6 @@ OUTDIR=$PWD/out
 mkdir -p $OUTDIR
 
 $ARDUINO_IDE/arduino-builder -build-path=$OUTDIR -hardware $ARDUINO_IDE/hardware/ -hardware $ARDUINO_LIB/packages -tools $ARDUINO_LIB/packages/esp8266/tools/ -tools $ARDUINO_IDE/tools-builder  -libraries=$LIBRARIES -fqbn=$FQBN main.ino
+
+echo Flash to device:
+echo esptool -cp /dev/cu.SLAB_USBtoUART -cb 921600 -cd nodemcu -cf out/main.ino.bin

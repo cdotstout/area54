f5167f71a2f5d5f0d44867f0c42672adb4d0fac6
cstout <castout@gmail.com>
2017-08-07 23:38:29 -0700

    Organize client code
    Build with: scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@ FQBN=esp8266:esp8266:nodemcuv2:CpuFrequency=80,UploadSpeed=921600,FlashSize=4M3M
 OUTDIR=$PWD/out
 mkdir -p $OUTDIR
 
-$ARDUINO_IDE/arduino-builder -build-path=$OUTDIR -hardware $ARDUINO_IDE/hardware/ -hardware $ARDUINO_LIB/packages -tools $ARDUINO_LIB/packages/esp8266/tools/ -tools $ARDUINO_IDE/tools-builder  -libraries=$LIBRARIES -fqbn=$FQBN main.ino
+$ARDUINO_IDE/arduino-builder -build-path=$OUTDIR -hardware $ARDUINO_IDE/hardware/ -hardware $ARDUINO_LIB/packages -tools $ARDUINO_LIB/packages/esp8266/tools/ -tools $ARDUINO_IDE/tools-builder  -libraries=$LIBRARIES -fqbn=$FQBN src/client/main.ino
 
 echo Flash to device:
 echo esptool -cp /dev/cu.SLAB_USBtoUART -cb 921600 -cd nodemcu -cf out/main.ino.bin

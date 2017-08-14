111155508ffbd3500257e3081912e40bd63a6f53
randy <whohat@gmail.com>
2017-08-14 01:17:08 -0700

    Change beacon address order and change default animation to simon

--- src/server/mqtt.py
+++ src/server/mqtt.py
@@ -4,12 +4,15 @@ import paho.mqtt.client as mqtt_client
 
 from animations import ANIMATIONS
 
-BEACON1 = 'area54/a020a61be29a'
-BEACON2 = 'area54/a020a616c8ed'
-BEACON3 = 'area54/a020a61bdf18'
-BEACON4 = 'area54/a020a61bc23f'
-BEACON5 = 'area54/a020a61bd9cc'
-BEACON6 = 'area54/6001941c7e9b'
+BEACON1 = 'area54/a020a61bdf18'
+BEACON2 = 'area54/a020a61bc23f'
+BEACON3 = 'area54/6001941c7e9b'
+BEACON4 = 'area54/a020a61bd9cc'
+BEACON5 = 'area54/a020a61be29a'
+BEACON6 = 'area54/a020a616c8ed'
+ALL_BEACONS = (BEACON1, BEACON2, BEACON3, BEACON4, BEACON5, BEACON6,)
+ODD_BEACONS = (BEACON1, BEACON3, BEACON5,)
+EVEN_BEACONS = (BEACON2, BEACON4, BEACON6, )
 
 
 class MqttClient(mqtt_client.Client):
@@ -23,7 +26,7 @@ class MqttClient(mqtt_client.Client):
         client.connect(cls.HOST)
         return client
 
-    def send_animation(self, address, animation='fuchsia'):
+    def send_animation(self, address, animation='simon'):
         """Send the animation to the beacon address."""
         self.publish(address, json.dumps(ANIMATIONS[animation]))
 

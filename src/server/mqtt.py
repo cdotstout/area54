2b442f5a38a38e74debb9bce545d218dc588d147
Brett M <whohat@gmail.com>
2017-08-14 12:30:42 -0700

    Clean up mqtt and update host

--- src/server/mqtt.py
+++ src/server/mqtt.py
@@ -1,13 +1,36 @@
+import json
+
 import paho.mqtt.client as mqtt_client
 
-HOST = 'pihole.deserts'
+from animations import ANIMATIONS
 
 
-def get_client():
-    """Create a mqtt client and connect to the server."""
-    client = mqtt_client.Client()
-    client.connect(HOST)
-    return client
+class MqttClient(mqtt_client.Client):
+
+    HOST = '192.168.44.10'
+    ADDRESSES = {
+        'a': 'area54/a020a61be29a',
+        's': 'area54/a020a616c8ed',
+        'd': 'area54/a020a61bdf18',
+        'f': 'area54/a020a61bc23f',
+        'g': 'area54/a020a61bd9cc',
+        'h': 'area54/a020a61bd8aa',
+    }
+
+    @classmethod
+    def get_client(cls):
+        """Initialize and connect."""
+        client = cls()
+        client.connect(cls.HOST)
+        return client
+
+    def get_address(self, step):
+        """Get the topic for the step."""
+        return self.ADDRESSES[step]
+
+    def send_animation(self, step, animation='fuchsia'):
+        """Send the animation to the beacon."""
+        self.publish(self.ADDRESSES[step], json.dumps(ANIMATIONS[animation]))
 
 
 if __name__ == '__main__':
--- src/server/sequencer.py
+++ src/server/sequencer.py
@@ -1,36 +1,31 @@
 import curses
-import json
 
-from animations import ANIMATIONS
-from mqtt import get_client
+from audio_player import AudioPlayer
+from mqtt import MqttClient
 
 
 class Sequencer:
 
-    ADDRESSES = {
-        'a': 'area54/a020a61be29a',
-        's': 'area54/a020a616c8ed',
-        'd': 'area54/a020a61bdf18',
-        'f': 'area54/a020a61bc23f',
-        'g': 'area54/a020a61bd9cc',
-        # 'h': 'area54/a020a61bd8aa',
-    }
-
-    def __init__(self, window):
-        self.mqtt_client = get_client()
+    def __init__(self, window, audio):
+        self.mqtt_client = MqttClient.get_client()
         self.window = window
+        self.audio_sequencer = None
+        if audio:
+            self.audio_sequencer = AudioPlayer()
 
-    def display_pattern(self, pattern, duration_between=300):
+    def display_pattern(self, pattern, duration_between=500):
         """Display the pattern with the beacons and in the window."""
         for step in pattern:
-            self.send_animation(step)
-            self.write_to_window(step)
+            self.play_step(step)
             curses.napms(duration_between)
         self.window.clear()
 
-    def send_animation(self, step, animation='fuchsia'):
-        """Send the animation to the beacon."""
-        self.mqtt_client.publish(self.ADDRESSES[step], json.dumps(ANIMATIONS[animation]))
+    def play_step(self, step, animation='fuchsia', audio_category='owens'):
+        """Play the LED animation and audio, and display the step."""
+        self.mqtt_client.send_animation(step, animation)
+        if self.audio_sequencer:
+            self.audio_sequencer.play_audio(step)
+        self.write_to_window(step)
 
     def write_to_window(self, step):
         """Write the step to the window."""
@@ -39,13 +34,15 @@ class Sequencer:
         self.window.refresh()
 
     def format_step(self, step):
-        """Pad the step."""
+        """Pad the step for display in the window."""
         return list(self.ADDRESSES).index(step) * ' ' + step
 
-    def display_game_over(self):
+    def game_over(self):
         """Send game over animation to beacons and window."""
         self.window.clear()
         self.window.addstr('GAME OVER')
         # TODO this should send 1 msg to a broadcast topic
         for address in self.ADDRESSES.values():
             self.mqtt_client.publish(address, json.dumps(ANIMATIONS['game_over']))
+        if self.audio_sequencer:
+            self.audio_sequencer.play_game_over()
